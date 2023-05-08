5a9a2998f44f6ad95fae604a4130ff5338f738ff
jsa0224 <voidness45@gmail.com>
2023-05-08 11:58:21 +0900

    feat: TabBarController 객체 추가

---
+++ Diary/Diary/Presentation/CommonView/View/TapBarController.swift
@@ -0,0 +1,56 @@
+//
+//  TapBarController.swift
+//  Diary
+//
+//  Created by 정선아 on 2023/05/02.
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureTabBar()
+    }
+
+    private func configureTabBar() {
+        tabBar.backgroundColor = .white
+        tabBar.tintColor = UIColor(named: "selectedColor")
+        tabBar.unselectedItemTintColor = UIColor(named: "mainColor")
+
+        let coreDataManager = CoreDataManager.shared
+        let repository = DiaryRepository(coreDataManager: coreDataManager)
+        let useCase = DefaultDiaryUseCase(diaryRepository: repository)
+        let homeViewModel = HomeViewModel(diaryUseCase: useCase)
+        let recordViewModel = RecordViewModel(diaryUseCase: useCase, diary: Diary(id: UUID(),
+                                                                                  title: "",
+                                                                                  body: "",
+                                                                                  createdAt: Date(),
+                                                                                  image: Data()))
+
+        let homeNavigationController = UINavigationController(rootViewController: HomeViewController(viewModel: homeViewModel))
+
+        let recordNavigationController = UINavigationController(rootViewController: RecordViewController(viewModel: recordViewModel))
+
+        homeNavigationController.navigationBar.scrollEdgeAppearance = homeNavigationController.navigationBar.standardAppearance
+
+        recordNavigationController.navigationBar.scrollEdgeAppearance = recordNavigationController.navigationBar.standardAppearance
+
+        viewControllers = [homeNavigationController, recordNavigationController]
+
+        let homeImage = UIImage(systemName: "house")
+        let homeTabBarItem = UITabBarItem(title: "home",
+                                          image: homeImage,
+                                          tag: 0)
+
+        let pencilImage = UIImage(systemName: "pencil.line")
+        let recordTabBarItem = UITabBarItem(title: "write",
+                                            image: pencilImage,
+                                            tag: 1)
+
+        homeNavigationController.tabBarItem = homeTabBarItem
+        recordNavigationController.tabBarItem = recordTabBarItem
+    }
+
+}
